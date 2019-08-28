7dcd05b7e82925dabf4a5c9fc1bd8666af7bb216
王水胜 <602466011@qq.com>
2019-08-28 17:07:01 +0800

    init

---
+++ src/components/slider.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    <!-- 如果需要滚动条 -->
+    <!-- <div class="swiper-scrollbar"></div> -->
+  </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {
+    this.$nextTick(() => {
+      new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: 2500,
+        pagination: '.swiper-pagination',
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        autoplayDisableOnInteraction: false,
+        preventClicks: false
+      })
+    })
+  }
+}
+</script>
+<style scoped>
+.swiper-container {
+  width: 600px;
+  height: 300px;
+  background-color: #1acd7e;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrapper">
+       <v-slider></v-slider> 
+    </div>
+</template>
+
+<script>
+import vSlider from '@/components/slider.vue'
+  export default {
+    components:{
+        vSlider
+    },
+    props:{},
+    data(){
+      return {
+          
+      }
+    },
+    watch:{},
+    computed:{},
+    methods:{},
+    created(){},
+    mounted(){}
+  }
+</script>
+<style scoped>
+</style>
